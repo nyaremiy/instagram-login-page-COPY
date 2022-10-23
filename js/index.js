189db550072488d6412edf241376138cad20a74f
NYaremiy <n.yaremiy791@gmail.com>
2022-10-24 00:29:53 +0300

    Added the authorization form

--- js/index.js
+++ js/index.js
@@ -36,36 +36,64 @@ const inputText = document.getElementById('js-input-text');
 const inputPass = document.getElementById('js-input-pass');
 const spanText = document.getElementById('js-span-text');
 const spanPass = document.getElementById('js-span-pass');
+const btnLogin = document.getElementById('js-btn-login');
+
+const form = {
+  username: '',
+  password: '',
+  disabled() {
+    if (+this.username.length > 0 && +this.password.length >= 6) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
 
 function startWriteInput(e, elementSpan, elementInput) {
+  changeDisabledBtn();
   if (e.target.value.length > 0) {
     elementSpan.classList.add('focus');
-    elementInput.style.paddingTop = '16px';
+    elementInput.style.paddingTop = '14px';
     if (elementInput === inputPass) {
       showPass.classList.add('show');
     }
   } else {
     elementSpan.classList.remove('focus');
-    elementInput.style.paddingTop = '9px';
+    elementInput.style.paddingTop = '0px';
     if (elementInput === inputPass) {
       showPass.classList.remove('show');
     }
   }
 }
 
-inputText.addEventListener('input', (e) =>
-  startWriteInput(e, spanText, inputText)
-);
-inputPass.addEventListener('input', (e) =>
-  startWriteInput(e, spanPass, inputPass)
-);
+inputText.addEventListener('input', (e) => {
+  form.username = e.target.value;
+  startWriteInput(e, spanText, inputText);
+});
+inputPass.addEventListener('input', (e) => {
+  form.password = e.target.value;
+  startWriteInput(e, spanPass, inputPass);
+});
 
 showPass.addEventListener('click', () => {
-  if (showPass.textContent === 'Показати') {
+  if (showPass.textContent.trim() === 'Показати') {
     inputPass.type = 'text';
     showPass.textContent = 'Сховати';
-  } else if (showPass.textContent === 'Сховати') {
+  } else if (showPass.textContent.trim() === 'Сховати') {
     inputPass.type = 'password';
     showPass.textContent = 'Показати';
   }
 });
+
+function changeDisabledBtn() {
+  if (form.disabled()) {
+    btnLogin.disabled = true;
+    btnLogin.classList.add('disabled');
+  } else {
+    btnLogin.disabled = false;
+    btnLogin.classList.remove('disabled');
+  }
+}
+
+changeDisabledBtn();
